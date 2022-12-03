f79cfce7697bdb4d4d774cc39709a74087fca77b
Andres Felipe Campuzano Cervantes <andrescampu1@gmail.com>
2022-12-02 21:50:16 -0500

    ahora se lista las pelicula de las lista, las peliculas se actualizan de estado.

--- src/pages/Dashboard/filsList/ViewList.jsx
+++ src/pages/Dashboard/filsList/ViewList.jsx
@@ -1,25 +1,142 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react'
+import { ButtonGroup, Card, Dropdown } from 'react-bootstrap';
 import { AiOutlinePlus } from 'react-icons/ai';
 import { useNavigate, useParams } from 'react-router';
+import Swal from 'sweetalert2';
 import MenuBar from '../../../components/Navbar'
 import axios from '../../../utils/connection';
-
 const ViewList = () => {
     const history = useNavigate();
     const params = useParams();
     const dataId = params.id;
     const [listFilms, setListFilms] = useState([]);
+    const [listStatus, setListStatus] = useState([]);
+
     useEffect(() => {
         const getFilms = () => {
             axios.post('/movie/getFilms', {
                 id_lista_pelicula: dataId
-            }).then(({data})=>{
-                setListFilms(data.films);
+            }).then(({ data }) => {
+                const list = data.films.map(item => ({
+                    ...item,
+                    fecha_lanzamiento: formatDate(item.fecha_lanzamiento.replace("T", " ").replace("Z", " "))
+                }))
+                setListFilms(list);
             }).catch(console.error)
         }
+        const getStatus = () => {
+            axios.get("/parameter/1")
+                .then(({ data }) => {
+                    if (data.parameter) {
+                        setListStatus(data.parameter.valoresParametros)
+                    }
+                });
+        }
         getFilms();
+        getStatus();
     }, [])
+
+    const formatDate = (date) => {
+        let d = new Date(date),
+            month = "" + (d.getMonth() + 1),
+            day = "" + d.getDate(),
+            year = d.getFullYear();
+        if (month.length < 2) month = "0" + month;
+
+        if (day.length < 2) day = "0" + day;
+
+        return [year, month, day,].join("-");
+    };
+
+    const deletemovie = (item) => {
+        Swal.fire({
+            title: 'Are you sure you want to delete the movie?',
+            showClass: {
+                popup: 'animate__animated animate__fadeInDown'
+            },
+            hideClass: {
+                popup: 'animate__animated animate__fadeOutUp'
+            },
+            showCancelButton: true,
+            cancelButtonColor: 'rgb(95 96 96)',
+            confirmButtonColor: '#d33',
+            confirmButtonText: 'Ok',
+            cancelButtonText: 'Cancel',
+            reverseButtons: true,
+            icon: 'warning',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axios.delete("/movie/" + item.id).then(({ data }) => {
+                    if (data.movieId.success) {
+                        const list = listFilms.filter(item2 => item2.id !== item.id);
+                        setListFilms(list);
+                        Swal.fire({
+                            title: 'Removed.',
+                            icon: 'success',
+                            timer: 600,
+                            showConfirmButton: false,
+                        })
+                    } else {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Oops...',
+                            text: 'Server connection error.',
+                            timer: 600,
+                        })
+                    }
+
+                }).catch((error) => {
+                    console.error(error);
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Server connection error.'
+                    })
+                })
+            }
+        })
+    }
+
+    const updateStatus = (item, status) => {
+        axios.put("/movie/" + item.id, { ...item, view_ED_parametro: status }).then(({ data }) => {
+            if (data.movieUpdated.success) {
+                Swal.fire({
+                    title: 'Updated.',
+                    icon: 'success',
+                    timer: 600,
+                    showConfirmButton: false,
+                }).then(() => {
+                    const list = listFilms.map(item => {
+                        if (item.id === data.movieUpdated.movieUpdated.id) {
+                            return{ 
+                                ...data.movieUpdated.movieUpdated, 
+                                fecha_lanzamiento: formatDate(data.movieUpdated.movieUpdated.fecha_lanzamiento.replace("T", " ").replace("Z", " "))
+                            }
+                        } else {
+                            return item
+                        }
+                    })
+                    setListFilms(list);
+                });
+            } else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: data.movieUpdated.message,
+                    timer: 600,
+                    showConfirmButton: false,
+                })
+            }
+        }).catch((error) => {
+            console.error(error);
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Server connection error.'
+            })
+        });
+    }
     return (
         <>
             <MenuBar />
@@ -43,14 +160,63 @@ const ViewList = () => {
                     />
                 </button>
                 <div>
-                {
-                    listFilms.length>0?<></>:
-                    <div className="div4 align-middle" style={{height:'80vh',padding:0}}>
-                        registers not finded.
-                    </div>
-                }
+                    {
+                        listFilms.length > 0 ?
+                            <div style={{ marginLeft: '5%', marginRight: '5%' }}>
+                                <div className="row justify-content-start">
+                                    {
+                                        listFilms.map((item) => (
+                                            <Card
+                                                key={item.id}
+                                                style={{
+                                                    width: 548.571428571,
+                                                    textAlign: 'justify',
+                                                    padding: 0,
+                                                    border: 'none',
+                                                    borderRadius: '5%',
+                                                    backgroundColor: item.view_ED_parametro===1?'#fff':item.view_ED_parametro===2?'#9fc5ff':'#666a70'
+                                                }}
+                                                className="m-2"
+                                            >
+                                                <Dropdown as={ButtonGroup}>
+                                                    <Dropdown.Toggle style={{
+                                                        position: 'absolute',
+                                                        right: 5,
+                                                        borderRadius: '50%',
+                                                        backgroundColor: '#efefef6e',
+                                                        top: 10,
+                                                        border: 'none',
+                                                        cursor: 'pointer',
+                                                    }}>
+                                                    </Dropdown.Toggle>
+                                                    <Dropdown.Menu>
+                                                        {
+                                                            listStatus.map(item2 => <Dropdown.Item key={item2.id} onClick={() => { updateStatus(item, item2.id) }}>{item2.valor_parametro}</Dropdown.Item>)
+                                                        }
+                                                        <Dropdown.Item onClick={() => { deletemovie(item) }}>Remove</Dropdown.Item>
+                                                    </Dropdown.Menu>
+                                                </Dropdown>
+                                                <img alt={item.nombre} width='100%' style={{ borderTopLeftRadius: '5%', borderTopRightRadius: '5%' }} src={`https://image.tmdb.org/t/p/original${item.path_imagen}`} />
+                                                <Card.Body>
+                                                    <Card.Title>{item.nombre}</Card.Title>
+                                                    <Card.Subtitle className="mb-2 text-muted">{item.fecha_lanzamiento}</Card.Subtitle>
+                                                    <Card.Text>
+                                                        {item.sinopsis}
+                                                    </Card.Text>
+                                                </Card.Body>
+
+                                            </Card>
+                                        ))
+                                    }
+                                </div>
+                            </div>
+                            :
+                            <div className="div4 align-middle" style={{ height: '80vh', padding: 0 }}>
+                                registers not finded.
+                            </div>
+                    }
                 </div>
-                
+
             </div>
         </>
     )
--- src/pages/Dashboard/searchMovies/index.js
+++ src/pages/Dashboard/searchMovies/index.js
@@ -1,21 +1,24 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import axios from 'axios'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect,  useState } from 'react'
 import { Card, Modal } from 'react-bootstrap';
 import MenuBar from '../../../components/Navbar'
 import axiosLocal from '../../../utils/connection'
 import "../../../styles/listStyle.css"
+import Swal from 'sweetalert2';
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
 const SearchMovies = () => {
     const [listMovies, setlistMovies] = useState([]);
     const api_key = "c2fdeb24edd254ed1f8d8a5997f683ea";
-    const page = useRef(1);
-    const total_pages = useRef(0);
+    const [page,setPage] =  useState(1);
+    const [total_pages,setTotal_pages] =  useState(0);
     const [listFilms, setListFilms] = useState([]);
     const [show, setShow] = useState(false)
     const [movie, setMovie] = useState({});
 
 
     const getMovies = () => {
-        axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&page=${page.current}`).then(({ data }) => {
+        axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&page=${page}`).then(({ data }) => {
 
             const list = data.results.map((item) => ({
                 id_pelicula: item.id,
@@ -25,7 +28,7 @@ const SearchMovies = () => {
                 fecha_lanzamiento: item.release_date,
             }))
             setlistMovies(list);
-            total_pages.current = data.total_pages;
+            setTotal_pages(data.total_pages);
         })
     }
     useEffect(() => {
@@ -38,9 +41,52 @@ const SearchMovies = () => {
             });
         }
         getList();
-    }, [])
+    }, [page])
 
     const ModalSelectList = ({ show, setShow, listFilms, movie }) => {
+
+        const hendelSelect = (id) => {
+            const data = {
+                id_lista_pelicula: id,
+                id_pelicula: movie.id_pelicula,
+                nombre: movie.nombre,
+                path_imagen: movie.path_imagen,
+                fecha_lanzamiento: movie.fecha_lanzamiento,
+                sinopsis: movie.sinopsis,
+                view_ED_parametro: 1
+            }
+            axiosLocal.post('/movie/', data)
+                .then(({ data }) => {
+                    if (data.movieCreated.success) {
+                        setShow(!show)
+                        Swal.fire({
+                            title: 'registered.',
+                            icon: 'success',
+                            timer: 600,
+                            showConfirmButton: false,
+                        })
+                    } else {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Oops...',
+                            text: data.movieCreated.message,
+                            timer: 600,
+                            showConfirmButton: false,
+                        })
+                    }
+                    console.log(data)
+                })
+                .catch((err) => {
+                    console.log(err)
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Server connection error.',
+                        timer: 600,
+                    })
+                });
+        }
+
         return (
             <Modal show={show} onHide={() => { setShow(!show) }}>
                 <Modal.Header closeButton>
@@ -50,9 +96,13 @@ const SearchMovies = () => {
                     <ul className='list-group'>
                         {
                             listFilms.map(item =>
-                            
-                                <li className='list-group-item activeItemLis' key={item.id}>{item.nombre}</li>
-                            
+                                <li
+                                    className='list-group-item activeItemLis mt-2'
+                                    onClick={() => { hendelSelect(item.id) }}
+                                    key={item.id}
+                                >
+                                    {item.nombre}
+                                </li>
                             )
                         }
                     </ul>
@@ -71,6 +121,45 @@ const SearchMovies = () => {
             <MenuBar />
             <ModalSelectList listFilms={listFilms} setShow={setShow} show={show} movie={movie} />
             <div style={{ marginLeft: '5%', marginRight: '5%' }}>
+                <div className="row d-flex justify-content-around align-items-center">
+                    <GrFormPrevious size={25} color="#fff"
+                        className='my-2 col-1  rounded-circle'
+                        onClick={() => {
+                            if (page>1) {
+                                setPage(page-1)
+                            }
+                        }}
+                        style={{
+                            width: 50,
+                            height: 50,
+                            cursor: 'pointer',
+                            backgroundColor: "#9fc5ff"
+                        }} />
+                    <span
+                        className='col-1 rounded-circle text-center fs-5 fw-bold'
+                        style={{
+                            backgroundColor: "#9fc5ff",
+                            width: 50,
+                            height: 50,
+                            paddingTop: 10,
+
+                        }}>
+                        {page}
+                    </span>
+                    <GrFormNext size={25}
+                        className='my-2 col-1  rounded-circle'
+                        onClick={() => {
+                            if (page<total_pages) {
+                                setPage(page+1)
+                            }
+                        }}
+                        style={{
+                            width: 50,
+                            height: 50,
+                            cursor: 'pointer',
+                            backgroundColor: "#9fc5ff"
+                        }} />
+                </div>
                 <div className="row justify-content-start">
                     {//https://image.tmdb.org/t/p/w220_and_h330_face/
                         listMovies.map((item) => (
